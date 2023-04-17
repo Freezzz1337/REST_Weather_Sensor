9370c0457be97580d01552a581adfbb04a6135a7
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-17 20:34:59 +0300

    add new model Sensor

---
+++ src/main/java/RestWeatherSensorApp/models/Sensor.java
@@ -0,0 +1,34 @@
+package RestWeatherSensorApp.models;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Sensor")
+public class Sensor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
