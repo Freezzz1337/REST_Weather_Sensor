64684b0fd456d0eea15b9e1dcef12ad1002bae7c
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-17 20:23:52 +0300

    start

---
+++ src/main/java/RestWeatherSensorApp/RestWeatherSensorApp.java
@@ -0,0 +1,13 @@
+package RestWeatherSensorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWeatherSensorApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWeatherSensorApp.class, args);
+	}
+
+}
