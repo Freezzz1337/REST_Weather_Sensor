944162974b72fdd8e5268c565ab50307cd4a304e
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-18 21:28:15 +0300

    add ExceptionHandler in SensorController

--- src/main/java/RestWeatherSensorApp/controllers/SensorController.java
+++ src/main/java/RestWeatherSensorApp/controllers/SensorController.java
@@ -4,12 +4,14 @@ import RestWeatherSensorApp.dto.SensorDTO;
 import RestWeatherSensorApp.models.Sensor;
 import RestWeatherSensorApp.services.SensorService;
 import RestWeatherSensorApp.util.SensorCreateException;
+import RestWeatherSensorApp.util.SensorErrorResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,7 @@ public class SensorController {
 
     private final SensorService sensorService;
     private final ModelMapper modelMapper;
+
     @Autowired
     public SensorController(SensorService sensorService, ModelMapper modelMapper) {
         this.sensorService = sensorService;
@@ -32,7 +35,7 @@ public class SensorController {
     public ResponseEntity<HttpStatus> sensorRegistration(@RequestBody @Valid SensorDTO sensorDTO,
                                                          BindingResult bindingResult) {
 
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
 
             List<FieldError> errors = bindingResult.getFieldErrors();
@@ -46,13 +49,21 @@ public class SensorController {
             throw new SensorCreateException(errorMsg.toString());
         }
 
-
         sensorService.save(convertToSensor(sensorDTO));
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
+    @ExceptionHandler
+    private ResponseEntity<SensorErrorResponse> handleException(SensorCreateException ex) {
+        SensorErrorResponse errorResponse = new SensorErrorResponse(
+                "Помика при створенні!",
+                System.currentTimeMillis()
+        );
 
-    private Sensor convertToSensor(SensorDTO sensorDTO){
-        return modelMapper.map(sensorDTO,Sensor.class);
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    private Sensor convertToSensor(SensorDTO sensorDTO) {
+        return modelMapper.map(sensorDTO, Sensor.class);
     }
 }
