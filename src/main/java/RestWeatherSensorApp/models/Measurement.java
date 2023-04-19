28cbe034f87fc8d1ae9f069a231567a075fc5aaa
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-19 18:44:47 +0300

    create Measurement model

---
+++ src/main/java/RestWeatherSensorApp/models/Measurement.java
@@ -0,0 +1,71 @@
+package RestWeatherSensorApp.models;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Measurement")
+public class Measurement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty
+    @Size(min = -100,max = 100)
+    @Column(name = "value")
+    private double value;
+
+    @NotEmpty
+    @Column(name = "raining")
+    private boolean raining;
+
+    @ManyToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    @Column(name = "sensor_id")
+    private Sensor sensor;
+
+    public Measurement() {
+    }
+
+    public Measurement(int id, double value, boolean raining, Sensor sensor) {
+        this.id = id;
+        this.value = value;
+        this.raining = raining;
+        this.sensor = sensor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
