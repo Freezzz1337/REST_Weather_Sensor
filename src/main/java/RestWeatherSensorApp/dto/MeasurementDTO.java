198e9f865411fb524972b485a3f9b766206a949e
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-21 22:54:13 +0300

    change MeasurementDTO

--- src/main/java/RestWeatherSensorApp/dto/MeasurementDTO.java
+++ src/main/java/RestWeatherSensorApp/dto/MeasurementDTO.java
@@ -1,7 +1,6 @@
 package RestWeatherSensorApp.dto;
 
 import RestWeatherSensorApp.models.Sensor;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
@@ -9,13 +8,21 @@ import javax.validation.constraints.NotNull;
 
 public class MeasurementDTO {
     @NotNull
-    @Range(min = -100,max = 100)
+    @Range(min = -100, max = 100)
     private double value;
     @NotNull
     private boolean raining;
 
     @NotNull
-    private int sensorId;
+    private Sensor sensor;
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
 
     public double getValue() {
         return value;
