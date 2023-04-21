d2b20452a9de4f5675fc95918aade12e0ed9c1cb
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-21 22:58:20 +0300

    change Sensor

--- src/main/java/RestWeatherSensorApp/models/Sensor.java
+++ src/main/java/RestWeatherSensorApp/models/Sensor.java
@@ -14,5 +14,11 @@ public class Sensor {
     @Size(min = 3, max = 30, message = "The name must be at least 3 and not more than 30 characters")
     private String name;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
