c7ce105988af0394c8c2838c55fb7c512121eeb7
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-21 22:48:25 +0300

    change model Sensor

--- src/main/java/RestWeatherSensorApp/models/Sensor.java
+++ src/main/java/RestWeatherSensorApp/models/Sensor.java
@@ -9,36 +9,10 @@ import javax.validation.constraints.Size;
 @Table(name = "Sensor")
 public class Sensor {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-
     @Column(name = "name")
     @NotEmpty
     @Size(min = 3, max = 30, message = "The name must be at least 3 and not more than 30 characters")
     private String name;
 
-    public Sensor(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Sensor() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
