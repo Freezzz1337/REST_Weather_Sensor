ed24f8258e1e87ba5481ac67c96dabe3add15058
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-21 18:22:02 +0300

    create MeasurementController

---
+++ src/main/java/RestWeatherSensorApp/controllers/MeasurementController.java
@@ -0,0 +1,11 @@
+package RestWeatherSensorApp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/measurements")
+public class MeasurementController {
+
+   
+}
