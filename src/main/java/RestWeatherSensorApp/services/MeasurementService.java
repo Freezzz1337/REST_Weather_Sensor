f4a4eaf97eafb4192203ca531b529e08beb1dcc5
Artem Samokysh <samokish777bombila@gmail.com>
2023-04-21 19:29:01 +0300

    add save() in MeasureService

--- src/main/java/RestWeatherSensorApp/services/MeasurementService.java
+++ src/main/java/RestWeatherSensorApp/services/MeasurementService.java
@@ -1,16 +1,26 @@
 package RestWeatherSensorApp.services;
 
+import RestWeatherSensorApp.models.Measurement;
 import RestWeatherSensorApp.repositories.MeasurementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Temporal;
 
 @Service
+@Transactional(readOnly = true)
 public class MeasurementService {
     private final MeasurementRepository measurementRepository;
 
     @Autowired
     public MeasurementService(MeasurementRepository measurementRepository) {
         this.measurementRepository = measurementRepository;
+    }
+
+    @Transactional
+    public void save(Measurement newMeasurement){
+        measurementRepository.save(newMeasurement);
     }
 
 
